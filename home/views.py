486469eeb975c8e19052c976f52a73dc4a09a321
Mrezagolbaba <m.reza.golbaba@gmail.com>
2023-04-04 18:50:14 +0300

    create home app

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def send_email(request):
+    person={'name':'reza','code':'1234321'}
+    return render(request,'email.html', context=person)
