a07287f929d7fad4eebd6e322be8c1581a9611e3
Mrezagolbaba <m.reza.golbaba@gmail.com>
2023-04-05 20:05:09 +0300

    update model and add to admin.py

--- home/views.py
+++ home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Todo
 
 
 # Create your views here.
 def send_email(request):
     person={'name':'reza','code':'1234321'}
     return render(request,'email.html', context=person)
+
+def home(request):
+    todos= Todo.objects.all()
+    return render(request ,'home.html',{'todos':todos})
